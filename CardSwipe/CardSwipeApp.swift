0870cd8fa2649c8e0e29082023c50f417018d2db
kehindeBankole <bankolek1@gmail.com>
2024-08-08 20:21:41 +0100

    Initial Commit

---
+++ CardSwipe/CardSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSwipeApp.swift
+//  CardSwipe
+//
+//  Created by Kehinde Bankole on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
